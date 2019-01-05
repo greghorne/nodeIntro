574a095081d494adfd2565f6432d80833f8c791a
Greg horne <greghorne@hotmail.com>
2019-01-05 02:33:18 +0000

    code updated

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express');
-var app = express();
-var fs = require("fs");
+var app     = express();
+var fs      = require("fs");
 
-app.get('/getdata', function (req, res) {
-    res.end('greg horne')
+app.get('/fname', function (req, res) {
+    res.end('greg')
+})
+
+app.get('/lname', function (req, res) {
+    res.end('horne')
+})
+
+app.get('/', function (req, res) {
+    res.end('app root')
 })
 
 var server = app.listen(3000, function () {
 //    var host = server.address().address
+   var env = process.env.APP_NAME;
    var host = '0.0.0.0';
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
+   console.log("APP_NAME: " + env);
 })
 
